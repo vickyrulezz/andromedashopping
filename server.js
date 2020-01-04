ef4f714cd8fb4e5b841f4e11ab0403f656ffc705
vickyrulezz <saha.vivek@gmail.com>
2020-01-04 15:33:28 +0530

    Updated shop.html

--- server.js
+++ server.js
@@ -241,6 +241,231 @@ console.log(sql);
   });
 });
 
+//GET ALL PRODUCTS - To retrieve all products - GRIDVIEW call this API ... URL/get_all_products
+app.get('/get_products_gridview',function(req, res) {
+
+    table = "";
+    //outData ='{${table}}';
+    outData = "OutputData";
+    resulthtml =`<html>
+                    <head>
+                        <meta charset="utf-8">
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    
+                        <!-- Mobile Metas -->
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                    
+                        <!-- Site Metas -->
+                        <title>KoolApp - Andromeda Shopping Kart</title>
+                        <meta name="keywords" content="">
+                        <meta name="description" content="">
+                        <meta name="author" content="Vivek Saha">
+                    
+                        <!-- Site Icons -->
+                        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+                        <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+                    
+                        <!-- Bootstrap CSS -->
+                        <link rel="stylesheet" href="css/bootstrap.min.css">
+                        <!-- Site CSS -->
+                        <link rel="stylesheet" href="css/style.css">
+                        <!-- Responsive CSS -->
+                        <link rel="stylesheet" href="css/responsive.css">
+                        <!-- Custom CSS -->
+                        <link rel="stylesheet" href="css/custom.css">
+                    </head>
+                    <body>
+                    `
+                    + outData +
+                    `
+                    <!-- ALL JS FILES -->
+                    <script src="js/jquery-3.2.1.min.js"></script>
+                    <script src="js/popper.min.js"></script>
+                    <script src="js/bootstrap.min.js"></script>
+                    <!-- ALL PLUGINS -->
+                    <script src="js/jquery.superslides.min.js"></script>
+                    <script src="js/bootstrap-select.js"></script>
+                    <script src="js/inewsticker.js"></script>
+                    <script src="js/bootsnav.js"></script>
+                    <script src="js/images-loded.min.js"></script>
+                    <script src="js/isotope.min.js"></script>
+                    <script src="js/owl.carousel.min.js"></script>
+                    <script src="js/baguetteBox.min.js"></script>
+                    <script src="js/form-validator.min.js"></script>
+                    <script src="js/contact-form-script.js"></script>
+                    <script src="js/custom.js"></script>
+    
+                    </body>
+                </html>`;
+    //resulthtml ='{${table}}';
+    //resulthtml ='<html><head><title>Kool App - Andromeda Product Page</title></head><body>{${table}}</body></html>';
+    
+    let sql = `select XXPC.COMMODITY_NAME PRODUCT_TYPE, XXSKU.ITEM_NUMBER SKU, XXPS.BRAND ,XXSKU.DESCRIPTION,XXSKU.LONG_DESCRIPTION, 
+    XXPR.LIST_PRICE,XXSKU.SKU_ATTRIBUTE_VALUE1 SIZE,XXSKU.SKU_ATTRIBUTE_VALUE2 COLOR,XXPR.IN_STOCK from 
+    XXIBM_PRODUCT_SKU XXSKU,
+    XXIBM_PRODUCT_PRICING XXPR,
+    XXIBM_PRODUCT_STYLE XXPS,
+    XXIBM_PRODUCT_CATALOGUE XXPC
+    where XXSKU.ITEM_NUMBER = XXPR.ITEM_NUMBER
+    and XXSKU.STYLE_ITEM = XXPS.ITEM_NUMBER
+    AND XXSKU.CATALOGUE_CATEGORY=XXPC.COMMODITY`;
+        
+    console.log(sql);
+      let query = mysqlClient.query(sql, (err, results, columns) => {
+        if(err) throw err;
+        
+        for(var i=0; i<results.length; i++){
+            table += `
+                        <div class="col-sm-6 col-md-6 col-lg-4 col-xl-4">
+                            <div class="products-single fix">
+                                <div class="box-img-hover">
+                                    <img src="images/product/`+results[i].SKU+`.jpg" class="img-fluid" alt="Image">
+                                    <div class="mask-icon">
+                                        <ul>
+                                            <li><a href="#" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
+                                            <li><a href="#" data-toggle="tooltip" data-placement="right" title="Compare"><i class="fas fa-sync-alt"></i></a></li>
+                                            <li><a href="#" data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li>
+                                        </ul>
+                                        <a class="cart" href="#">Add to Cart</a>
+                                    </div>
+                                </div>
+                                <div class="why-text">
+                                    <span>`+ results[i].DESCRIPTION +`</span><br/>
+                                    <span>`+ results[i].PRODUCT_TYPE +`&emsp;&emsp;`+ results[i].BRAND +`</span><br/>
+                                    <span>`+ results[i].LONG_DESCRIPTION +`<span><br/>
+                                    <span> ₹ `+ results[i].LIST_PRICE +`</span><br/>
+                                    <span>`+ results[i].COLOR +`&emsp;&emsp;`+ results[i].SIZE +`</span><br/>
+                                    <span> In Stock : `+ results[i].IN_STOCK +`</span>
+                                </div>
+                            </div>
+                        </div>
+                        `;
+        }
+           table =`                 <div class="row">`
+                                        + table +
+                                    `</div>     `;
+        
+        //resulthtml = resulthtml.replace('{${table}}', table);
+        resulthtml = resulthtml.replace('OutputData', table);
+        console.log(resulthtml);
+        res.send(resulthtml);
+      });
+    });
+    
+
+//GET ALL PRODUCTS - To retrieve all products - LISTVIEW call this API ... URL/get_all_products
+app.get('/get_products_listview',function(req, res) {
+
+    table = "";
+    //outData ='{${table}}';
+    outData = "OutputData";
+    resulthtml =`<html>
+                    <head>
+                        <meta charset="utf-8">
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    
+                        <!-- Mobile Metas -->
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                    
+                        <!-- Site Metas -->
+                        <title>KoolApp - Andromeda Shopping Kart</title>
+                        <meta name="keywords" content="">
+                        <meta name="description" content="">
+                        <meta name="author" content="Vivek Saha">
+                    
+                        <!-- Site Icons -->
+                        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+                        <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+                    
+                        <!-- Bootstrap CSS -->
+                        <link rel="stylesheet" href="css/bootstrap.min.css">
+                        <!-- Site CSS -->
+                        <link rel="stylesheet" href="css/style.css">
+                        <!-- Responsive CSS -->
+                        <link rel="stylesheet" href="css/responsive.css">
+                        <!-- Custom CSS -->
+                        <link rel="stylesheet" href="css/custom.css">
+                    </head>
+                    <body>
+                    `
+                    + outData +
+                    `
+                    <!-- ALL JS FILES -->
+                    <script src="js/jquery-3.2.1.min.js"></script>
+                    <script src="js/popper.min.js"></script>
+                    <script src="js/bootstrap.min.js"></script>
+                    <!-- ALL PLUGINS -->
+                    <script src="js/jquery.superslides.min.js"></script>
+                    <script src="js/bootstrap-select.js"></script>
+                    <script src="js/inewsticker.js"></script>
+                    <script src="js/bootsnav.js"></script>
+                    <script src="js/images-loded.min.js"></script>
+                    <script src="js/isotope.min.js"></script>
+                    <script src="js/owl.carousel.min.js"></script>
+                    <script src="js/baguetteBox.min.js"></script>
+                    <script src="js/form-validator.min.js"></script>
+                    <script src="js/contact-form-script.js"></script>
+                    <script src="js/custom.js"></script>
+    
+                    </body>
+                </html>`;
+    
+    let sql = `select XXPC.COMMODITY_NAME PRODUCT_TYPE, XXSKU.ITEM_NUMBER SKU, XXPS.BRAND ,XXSKU.DESCRIPTION,XXSKU.LONG_DESCRIPTION, 
+    XXPR.LIST_PRICE,XXSKU.SKU_ATTRIBUTE_VALUE1 SIZE,XXSKU.SKU_ATTRIBUTE_VALUE2 COLOR,XXPR.IN_STOCK from 
+    XXIBM_PRODUCT_SKU XXSKU,
+    XXIBM_PRODUCT_PRICING XXPR,
+    XXIBM_PRODUCT_STYLE XXPS,
+    XXIBM_PRODUCT_CATALOGUE XXPC
+    where XXSKU.ITEM_NUMBER = XXPR.ITEM_NUMBER
+    and XXSKU.STYLE_ITEM = XXPS.ITEM_NUMBER
+    AND XXSKU.CATALOGUE_CATEGORY=XXPC.COMMODITY`;
+        
+    console.log(sql);
+      let query = mysqlClient.query(sql, (err, results, columns) => {
+        if(err) throw err;
+        
+        for(var i=0; i<results.length; i++){
+            table += `
+                        <div class="col-sm-6 col-md-6 col-lg-4 col-xl-4">
+                            <div class="products-single fix">
+                                <div class="box-img-hover">
+                                    <img src="images/product/`+results[i].SKU+`.jpg" class="img-fluid" alt="Image">
+                                    <div class="mask-icon">
+                                        <ul>
+                                            <li><a href="#" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
+                                            <li><a href="#" data-toggle="tooltip" data-placement="right" title="Compare"><i class="fas fa-sync-alt"></i></a></li>
+                                            <li><a href="#" data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li>
+                                        </ul>
+                                        <a class="cart" href="#">Add to Cart</a>
+                                    </div>
+                                </div>
+                                <div class="why-text">
+                                    <span>`+ results[i].DESCRIPTION +`</span><br/>
+                                    <span>`+ results[i].PRODUCT_TYPE +`&emsp;&emsp;`+ results[i].BRAND +`</span><br/>
+                                    <span>`+ results[i].LONG_DESCRIPTION +`<span><br/>
+                                    <span> ₹ `+ results[i].LIST_PRICE +`</span><br/>
+                                    <span>`+ results[i].COLOR +`&emsp;&emsp;`+ results[i].SIZE +`</span><br/>
+                                    <span> In Stock : `+ results[i].IN_STOCK +`</span>
+                                </div>
+                            </div>
+                        </div>
+                        `;
+            //table +='<tr><td>'+ (i+1) +'</td><td>'+ results[i].PRODUCT_TYPE +'</td><td>'+ results[i].SKU +'</td><td>'+ results[i].BRAND +'</td><td>'+ results[i].DESCRIPTION +'</td><td>'+ results[i].LONG_DESCRIPTION +'</td><td>'+ results[i].LIST_PRICE +'</td><td>'+ results[i].SIZE +'</td><td>'+ results[i].COLOR+'</td><td>'+ results[i].IN_STOCK +'</td></tr>';
+        }
+           table =`
+                                <div class="list-view-box">
+                                    <div class="row">`
+                                        + table +
+                                    `</div>
+                                </div>
+                `;
+        
+        //resulthtml = resulthtml.replace('{${table}}', table);
+        resulthtml = resulthtml.replace('OutputData', table);
+        console.log(resulthtml);
+        res.send(resulthtml);
+      });
+    });
 
 
 // Port Listen
