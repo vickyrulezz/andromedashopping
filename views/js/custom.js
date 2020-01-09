8a8150e854fce4a43bc2927b93a4a89a1ed11bb3
vickyrulezz <saha.vivek@gmail.com>
2020-01-09 16:29:31 +0530

    code changes

--- views/js/custom.js
+++ views/js/custom.js
@@ -194,14 +194,14 @@
 		$("#slider-range").slider({
 			range: true,
 			min: 0,
-			max: 4000,
+			max: 5000,
 			values: [1000, 3000],
 			slide: function(event, ui) {
-				$("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+				$("#amount").val("₹ " + ui.values[0] + " - ₹ " + ui.values[1]);
 			}
 		});
-		$("#amount").val("$" + $("#slider-range").slider("values", 0) +
-			" - $" + $("#slider-range").slider("values", 1));
+		$("#amount").val("₹ " + $("#slider-range").slider("values", 0) +
+			" - ₹ " + $("#slider-range").slider("values", 1));
 	});
 
 	/* ..............................................
